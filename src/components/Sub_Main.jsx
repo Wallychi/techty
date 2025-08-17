eddf5d7d22f0db890826d08c22397e6afe8444bb
Wallychi <szetrov@gmail.com>
2025-08-17 05:32:42 +0100

    add state and onclick to the changable part of the sub_main

--- src/components/Sub_Main.jsx
+++ src/components/Sub_Main.jsx
@@ -1,36 +1,215 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "./Button";
 
 const Sub_Main = () => {
-  return (
-    <section className='flex flex-col font-space w-full'>
-        <div className='flex items-center justify-center py-25'>
-            <p className='text-center text-5xl font-extrabold'>Empower your business<br /> with cutting-edge <span className='text-green-500'>solutions</span></p>
-        </div>
-        <div className='flex mx-15 border-b-1 border-b-gray-300 mb-10 gap-2'>
-            <div className='flex items-center gap-3 pb-7'>
-                <p className='text-6xl font-extrabold'>01</p>
-                <div className='flex flex-col gap-2 '>
-                    <p className='font-semibold text-lg'>Collect Ideas</p>
-                    <p>Creating or improving products to meet our user needs.</p>
-                </div>
-            </div>
-            <div className='flex items-center gap-3 pb-7'>
-                <p className='font-extrabold text-6xl'>02</p>
-                <div className='flex flex-col gap-2'>
-                    <p className='font-semibold text-lg'>Data Analysis</p>
-                    <p>Creating or improving products to meet our user needs.</p>
-                </div>
-            </div>
-            <div className='flex items-center gap-3 pb-7'>
-                <p className='font-extrabold text-6xl'>03</p>
-                <div className='flex flex-col gap-2'>
-                    <p className='font-semibold text-lg'>Analyze Products</p>
-                    <p>Creating or improving products to meet our user needs.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-  )
-}
+  const [first, setFirst] = useState(true);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
 
-export default Sub_Main
+  return (
+    <section className="flex flex-col font-space w-full">
+      <div className="flex items-center justify-center py-25">
+        <p className="text-center text-5xl font-extrabold">
+          Empower your business
+          <br /> with cutting-edge{" "}
+          <span className="text-green-500">solutions</span>
+        </p>
+      </div>
+      <div className="flex mx-15 border-b-1 border-b-gray-300 mb-10 gap-2">
+        <div
+          onClick={() => {
+            setFirst(true), setSecond(false), setThird(false);
+          }}
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center gap-3 pb-7"
+              : "flex items-center gap-3 pb-7"
+          }
+        >
+          <h1 className="text-6xl font-extrabold">01</h1>
+          <div className="flex flex-col gap-2 ">
+            <p className="font-semibold text-lg">Collect Ideas</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
+        </div>
+        <div
+          onClick={() => {
+            setFirst(false), setSecond(true), setThird(false);
+          }}
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center gap-3 pb-7"
+              : "flex items-center gap-3 pb-7"
+          }
+        >
+          <h1 className="font-extrabold text-6xl">02</h1>
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold text-lg">Data Analysis</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
+        </div>
+        <div
+          onClick={() => {
+            setFirst(false), setSecond(false), setThird(true);
+          }}
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center gap-3 pb-7"
+              : "flex items-center gap-3 pb-7"
+          }
+        >
+          <h1 className="font-extrabold text-6xl">03</h1>
+          <div className="flex flex-col gap-2">
+            <p className="font-semibold text-lg">Analyze Products</p>
+            <p>Creating or improving products to meet our user needs.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        {first ? (
+          <div className="flex items-center  mx-15 gap-10">
+            <img
+              src="/images/Frame 29.png"
+              alt="image of a phone and laptop"
+              className="w-[550px]"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Collect Ideas</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {second ? (
+          <div className="flex items-center  mx-15 mb-10 gap-10">
+            <img
+              src="/images/data_analysis.JPG"
+              alt="image of pencil, paper and book"
+              className="w-[550px]"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Data Analysis</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {third ? (
+          <div className="flex items-center  mx-15 mb-10 gap-10">
+            <img
+              src="/images/finalize_project.JPG"
+              alt="image of 8 colored papers"
+              className="w-[550px]"
+            />
+            <div className="flex flex-col gap-7">
+              <h1 className="text-3xl font-semibold">Finalize Product</h1>
+              <p className="text-md text-[#4E4E4E] text-justify">
+                With our Techty company, you can create landing pages that
+                convert more <br /> visitors than any other website. You can
+                easily create a page using a variety <br /> of unique blocks.
+              </p>
+              <div className="flex flex-col gap-3 ">
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    We collect ideas from different design inspirations,
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Analysis data for any kind of corrections.
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <img src="/icons/checkbox-circle-line.png" />
+                  <p className="text-xl">
+                    Finalize the product for the production to be done.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <Button
+                  name="Request a Free Demo"
+                  style={"bg-black text-white px-5 hover:bg-green-500"}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+
+      <div className="flex w-full h-screen">
+        <div className="flex items-center bg-green -500"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Sub_Main;
